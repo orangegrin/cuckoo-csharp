d1eb5e5fb13ac02426e2bccac82fa93a5e748317
henry <xbhuang1994@gmail.com>
2019-03-15 17:09:38 +0800

    clear key builder

--- cuckoo-csharp/Program.cs
+++ cuckoo-csharp/Program.cs
@@ -14,8 +14,6 @@ namespace cuckoo_csharp
 
         static void Main(string[] args)
         {
-
-            BuildKeys();
             var config1 = GetCrossMarketConfig();
             config1.MinIRS = 0.0012m;
             config1.MaxQty = 100;
@@ -32,26 +30,6 @@ namespace cuckoo_csharp
             {
                 Console.ReadLine();
             }
-        }
-
-        static void BuildKeys()
-        {
-            //BITMEX Account 1:
-            //string publickey = "2xrwtDdMimp5Oi3F6oSmtsew";
-            //string privatekey = "rxgzE8FCETaWXxXAXe5daqxRJWshqJoD-ERIipxdC_H2hexs";
-
-            //BITMEX Account 2:
-            //name: t295202690@gmail.com
-            //pass: tii540105249
-            string publickey = "2xrwtDdMimp5Oi3F6oSmtsew";
-            string privatekey = "rxgzE8FCETaWXxXAXe5daqxRJWshqJoD-ERIipxdC_H2hexs";
-
-            //string publickey = "v_eancoUBO7lRJf9_AXsCGbg";
-            //string privatekey = "jBLZ2aUdS9U-LDP5ZvBrItW1Rg6akLgNGqUQCuQq3wK0HRS4";
-            CryptoUtility.SaveUnprotectedStringsToFile(ExchangeName.BitMEX, new string[2] { publickey, privatekey });
-            string publickey2 = "440757a5-e78ac402-84903e36-194b1";
-            string privatekey2 = "7f0a0c5c-24fd0bb9-eb64134f-2e1b6";
-            CryptoUtility.SaveUnprotectedStringsToFile(ExchangeName.HBDM, new string[2] { publickey2, privatekey2 });
         }
 
         static CrossMarketConfig GetCrossMarketConfig()
