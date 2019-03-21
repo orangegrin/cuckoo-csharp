5a2e623c88fb9bfd3c13d289d86673d8b03e04d3
henry <xbhuang1994@gmail.com>
2019-03-21 14:50:29 +0800

    start ladderback

--- cuckoo-csharp/Program.cs
+++ cuckoo-csharp/Program.cs
@@ -14,18 +14,22 @@ namespace cuckoo_csharp
 
         static void Main(string[] args)
         {
-            var config1 = GetCrossMarketConfig();
-            config1.MinIRS = 0.001m;
-            config1.MaxQty = 100;
-            new CrossMarket(config1,1).Start();
-            var config2 = GetCrossMarketConfig();
-            config2.MinIRS = 0.0024m;
-            config2.MaxQty = 200;
-            new CrossMarket(config2,2).Start();
-            var config3 = GetCrossMarketConfig();
-            config3.MinIRS = 0.0042m;
-            config3.MaxQty = 400;
-            new CrossMarket(config3,3).Start();
+            //CryptoUtility.SaveUnprotectedStringsToFile(ExchangeName.BitMEX, "6hj439I7D1v3455ThTZf-Xe4", "6NI1qwEaQfFxPTKM01_Du6DWiJpZiH9dDE_8dGnXOIybMdpY");
+            CryptoUtility.SaveUnprotectedStringsToFile(ExchangeName.BitMEX, "pFUlk3M9J3n1DGMQt4cMNxOr", "7JPonP208D_JM7wnp_kv1xupKdTwMznB5z1oVgNTGmphav1Q");
+            LadderBack lb = new LadderBack(new LadderBack.Config { ExchangeName = ExchangeName.BitMEX, Symbol = "XBTUSD", Step = 1m, KeepValue = 100000m, MinPriceUnit = 0.5m });
+            lb.Start();
+            //var config1 = GetCrossMarketConfig();
+            //config1.MinIRS = 0.001m;
+            //config1.MaxQty = 100;
+            //new CrossMarket(config1,1).Start();
+            //var config2 = GetCrossMarketConfig();
+            //config2.MinIRS = 0.0024m;
+            //config2.MaxQty = 200;
+            //new CrossMarket(config2,2).Start();
+            //var config3 = GetCrossMarketConfig();
+            //config3.MinIRS = 0.0042m;
+            //config3.MaxQty = 400;
+            //new CrossMarket(config3,3).Start();
             while (true)
             {
                 Console.ReadLine();
