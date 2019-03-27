1799488b2da52f25f93498b21fc2bde0673cc63e
Henry <xbhuang1994@gmail.com>
2019-03-27 19:04:11 +0800

    add intertemporal

--- cuckoo-csharp/Program.cs
+++ cuckoo-csharp/Program.cs
@@ -16,18 +16,22 @@ namespace cuckoo_csharp
         {
 
             BuildKeys();
-            var config1 = GetCrossMarketConfig();
-            config1.MinIRS = 0.0012m;
-            config1.MaxQty = 100;
-            //new CrossMarket(config1).Start();
-            var config2 = GetCrossMarketConfig();
-            config2.MinIRS = 0.0024m;
-            config2.MaxQty = 200;
-            //new CrossMarket(config2).Start();
-            var config3 = GetCrossMarketConfig();
-            config3.MinIRS = 0.0042m;
-            config3.MaxQty = 400;
-            new CrossMarket(config3).Start();
+            //var config1 = GetCrossMarketConfig();
+            //config1.MinIRS = 0.0012m;
+            //config1.MaxQty = 100;
+            ////new CrossMarket(config1).Start();
+            //var config2 = GetCrossMarketConfig();
+            //config2.MinIRS = 0.0024m;
+            //config2.MaxQty = 200;
+            ////new CrossMarket(config2).Start();
+            //var config3 = GetCrossMarketConfig();
+            //config3.MinIRS = 0.0042m;
+            //config3.MaxQty = 400;
+            //new CrossMarket(config3).Start();
+
+            var config = new Intertemporal.IntertemporalConfig() { ExchangeNameA = ExchangeName.BitMEX, ExchangeNameB = ExchangeName.Binance, SymbolA = "XBTH19", SymbolB = "BTC_USDT", MaxQty = 50, OPDF = 0.008m, CPDF = 0.003m, PerTrans = 10 };
+
+            new Intertemporal(config).Start();
             while (true)
             {
                 Console.ReadLine();
@@ -45,16 +49,22 @@ namespace cuckoo_csharp
             //pass: tii540105249
             string publickey = "2xrwtDdMimp5Oi3F6oSmtsew";
             string privatekey = "rxgzE8FCETaWXxXAXe5daqxRJWshqJoD-ERIipxdC_H2hexs";
+            CryptoUtility.SaveUnprotectedStringsToFile(ExchangeName.BitMEX, new string[2] { publickey, privatekey });
+
+            string publickey2 = "GXKrqpqZXCnhJ82nhy7MwzPcYGIVtKyd9EtHcVGauxlOVSutTFyNwoa5yn6bteVO";
+            string privatekey2 = "GtwcSOpV51tYAt8LeOGYDd31a7r7zmjerwYDyyBlRYBaUgw2tUFc0MtyaOvKQ6PR";
+            CryptoUtility.SaveUnprotectedStringsToFile(ExchangeName.Binance, new string[2] { publickey2, privatekey2 });
+
 
             //string publickey = "v_eancoUBO7lRJf9_AXsCGbg";
             //string privatekey = "jBLZ2aUdS9U-LDP5ZvBrItW1Rg6akLgNGqUQCuQq3wK0HRS4";
-            CryptoUtility.SaveUnprotectedStringsToFile(ExchangeName.BitMEX, new string[2] { publickey, privatekey });
-            string publickey2 = "440757a5-e78ac402-84903e36-194b1";
-            string privatekey2 = "7f0a0c5c-24fd0bb9-eb64134f-2e1b6";
-            CryptoUtility.SaveUnprotectedStringsToFile(ExchangeName.HBDM, new string[2] { publickey2, privatekey2 });
-            string publickey3 = "52442d6f217c94e99ee10581c050598f";
-            string privatekey3 = "3a565f3b7873aaab6f8c6d17135d6dde484c14119c15080887047e16ac91e8ef";
-            CryptoUtility.SaveUnprotectedStringsToFile(ExchangeName.GateioDM, new string[2] { publickey3, privatekey3 });
+            //CryptoUtility.SaveUnprotectedStringsToFile(ExchangeName.BitMEX, new string[2] { publickey, privatekey });
+            //string publickey2 = "440757a5-e78ac402-84903e36-194b1";
+            //string privatekey2 = "7f0a0c5c-24fd0bb9-eb64134f-2e1b6";
+            //CryptoUtility.SaveUnprotectedStringsToFile(ExchangeName.HBDM, new string[2] { publickey2, privatekey2 });
+            //string publickey3 = "52442d6f217c94e99ee10581c050598f";
+            //string privatekey3 = "3a565f3b7873aaab6f8c6d17135d6dde484c14119c15080887047e16ac91e8ef";
+            //CryptoUtility.SaveUnprotectedStringsToFile(ExchangeName.GateioDM, new string[2] { publickey3, privatekey3 });
         }
         static CrossMarketConfig GetCrossMarketConfig()
         {
---
+++ cuckoo-csharp/Strategy/Arbitrage/Intertemporal.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ExchangeSharp;
+
+namespace cuckoo_csharp.Strategy.Arbitrage
+{
+    public class Intertemporal
+    {
+        public IntertemporalConfig mConfig;
+        private IExchangeAPI mExchangeAAPI;
+        private IExchangeAPI mExchangeBAPI;
+        private ExchangeOrderBook mOrderBookA;
+        private ExchangeOrderBook mOrderBookB;
+        private decimal curAmount = 0;
+        public Intertemporal(IntertemporalConfig config)
+        {
+            mConfig = config;
+            mExchangeAAPI = ExchangeAPI.GetExchangeAPI(mConfig.ExchangeNameA);
+            mExchangeBAPI = ExchangeAPI.GetExchangeAPI(mConfig.ExchangeNameB);
+
+        }
+
+        public void Start()
+        {
+            mExchangeAAPI.LoadAPIKeys(mConfig.ExchangeNameA);
+            mExchangeBAPI.LoadAPIKeys(mConfig.ExchangeNameB);
+            mExchangeAAPI.GetFullOrderBookWebSocket(OnOrderbookAHandler, 20, mConfig.SymbolA);
+            mExchangeBAPI.GetFullOrderBookWebSocket(OnOrderbookBHandler, 20, mConfig.SymbolB);
+        }
+
+        private void OnOrderbookAHandler(ExchangeOrderBook order)
+        {
+            mOrderBookA = order;
+            OnOrderBookHandler();
+
+
+        }
+
+        private void OnOrderbookBHandler(ExchangeOrderBook order)
+        {
+            mOrderBookB = order;
+            OnOrderBookHandler();
+        }
+
+        private Task mRunningTask;
+
+        async void OnOrderBookHandler()
+        {
+            if (mOrderBookA == null || mOrderBookB == null)
+                return;
+            if (mRunningTask != null && !mRunningTask.IsCompleted)
+                return;
+            mRunningTask = Task.Delay(1000 * 5);
+            decimal exchangeAmount;
+            decimal buyPrice;
+            decimal sellPrice;
+            mOrderBookA.GetPriceToBuy(mConfig.PerTrans, out exchangeAmount, out buyPrice);
+            exchangeAmount = Math.Round(exchangeAmount, 6);
+            sellPrice = mOrderBookB.GetPriceToSell(exchangeAmount);
+            if (buyPrice == 0 || sellPrice == 0 || exchangeAmount == 0)
+                return;
+            Console.WriteLine("================================================");
+            Console.WriteLine(sellPrice / buyPrice - 1);
+            Console.WriteLine("{0} {1} {2}", buyPrice, sellPrice, exchangeAmount);
+            if (sellPrice / buyPrice - 1 > mConfig.OPDF && Math.Abs(curAmount) < mConfig.MaxQty)
+            {
+                OpenPosition(exchangeAmount);
+            }
+            mOrderBookB.GetPriceToBuy(mConfig.PerTrans, out exchangeAmount, out buyPrice);
+            sellPrice = mOrderBookA.GetPriceToSell(exchangeAmount);
+            Console.WriteLine(1 - sellPrice / buyPrice);
+            Console.WriteLine("{0} {1} {2}", buyPrice, sellPrice, exchangeAmount);
+            if (1 - sellPrice / buyPrice < mConfig.CPDF && Math.Abs(curAmount) < mConfig.MaxQty)
+            {
+                ClosePosition(exchangeAmount);
+            }
+            await mRunningTask;
+        }
+        private async void OpenPosition(decimal exchangeAmount)
+        {
+
+            try
+            {
+                //开仓
+                ExchangeOrderRequest requestA = new ExchangeOrderRequest();
+                requestA.Amount = mConfig.PerTrans;
+                requestA.MarketSymbol = mConfig.SymbolA;
+                requestA.IsBuy = true;
+                requestA.OrderType = OrderType.Market;
+                ExchangeOrderRequest requestB = new ExchangeOrderRequest();
+                requestB.Amount = exchangeAmount;
+                requestB.MarketSymbol = mConfig.SymbolB;
+                requestB.IsBuy = false;
+                requestB.OrderType = OrderType.Market;
+
+                var orderA = await mExchangeAAPI.PlaceOrderAsync(requestA);
+                if (orderA.Result == ExchangeAPIOrderResult.Filled)
+                {
+                    curAmount += mConfig.PerTrans;
+                    var orderB = await mExchangeBAPI.PlaceOrderAsync(requestB);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private async void ClosePosition(decimal exchangeAmount)
+        {
+            try
+            {
+                //开仓
+                ExchangeOrderRequest requestA = new ExchangeOrderRequest();
+                requestA.Amount = mConfig.PerTrans;
+                requestA.MarketSymbol = mConfig.SymbolA;
+                requestA.IsBuy = false;
+                requestA.OrderType = OrderType.Market;
+                ExchangeOrderRequest requestB = new ExchangeOrderRequest();
+                requestB.Amount = exchangeAmount;
+                requestB.MarketSymbol = mConfig.SymbolB;
+                requestB.IsBuy = true;
+                requestB.OrderType = OrderType.Market;
+                var orderA = await mExchangeAAPI.PlaceOrderAsync(requestA);
+                if (orderA.Result == ExchangeAPIOrderResult.Filled)
+                {
+                    curAmount -= mConfig.PerTrans;
+                    var orderB = await mExchangeBAPI.PlaceOrderAsync(requestB);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public class IntertemporalConfig
+        {
+            public string ExchangeNameA;
+            public string ExchangeNameB;
+            public string SymbolA;
+            public string SymbolB;
+            public decimal MaxQty;
+            /// <summary>
+            /// 开仓差
+            /// </summary>
+            public decimal OPDF;
+            /// <summary>
+            /// 平仓差
+            /// </summary>
+            public decimal CPDF;
+            public decimal PerTrans;
+        }
+    }
+}
