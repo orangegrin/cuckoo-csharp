abeeb50148608589cd76508d06adb2edc46ea6b3
liuqiba <874740127@qq.com>
2019-05-23 23:40:18 +0800

    期对期交易

---
+++ cuckoo-csharp/Tools/RedisDB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StackExchange.Redis;
+using Newtonsoft.Json;
+
+namespace cuckoo_csharp.Tools
+{
+    public static class RedisDB
+    {
+        private static ConnectionMultiplexer connection;
+        private static IDatabase instance;
+
+        public static IDatabase Instance
+        {
+            get
+            {
+                if (connection == null || !connection.IsConnected)
+                {
+                    //connection = ConnectionMultiplexer.Connect("localhost");
+                    //instance = connection.GetDatabase();
+                    throw new Exception("请先使用 Init初始化连接");
+                }
+                return instance;
+            }
+        }
+        public static void Init(string configStr)
+        {
+            if (connection == null || !connection.IsConnected)
+            {
+                connection = ConnectionMultiplexer.Connect(configStr);
+                instance = connection.GetDatabase();
+            }
+        }
+
+
+        public static T StringGet<T>(this IDatabase database, RedisKey key, CommandFlags flags = CommandFlags.None)
+        {
+            string res = database.StringGet(key, flags);
+            if (res == null)
+                return default(T);
+            return JsonConvert.DeserializeObject<T>(res);
+        }
+        public static bool StringSet(this IDatabase database, RedisKey key, object value, TimeSpan? expiry = null, When when = When.Always, CommandFlags flags = CommandFlags.None)
+        {
+            var val = JsonConvert.SerializeObject(value);
+            return database.StringSet(key, val, expiry, when, flags);
+        }
+    }
+
+}
