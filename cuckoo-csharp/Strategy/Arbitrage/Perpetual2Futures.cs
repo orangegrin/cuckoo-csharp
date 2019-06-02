e881138c576696a1c444a045cb8193c195740baa
liuqiba <874740127@qq.com>
2019-06-02 17:27:53 +0800

    fix bug

--- cuckoo-csharp/Strategy/Arbitrage/Perpetual2Futures.cs
+++ cuckoo-csharp/Strategy/Arbitrage/Perpetual2Futures.cs
@@ -346,10 +346,10 @@ namespace cuckoo_csharp.Strategy.Arbitrage
                 var v = await mExchangeAAPI.PlaceOrdersAsync(requestA);
                 mCurOrderA = v[0];
                 mOrderIds.Add(mCurOrderA.OrderId);
-                if (mCurOrderA.Result == ExchangeAPIOrderResult.Canceled)
-                    mCurOrderA = null;
                 Logger.Debug("mId:" + mId + "requestA：  " + requestA.ToString());
                 Logger.Debug("mId:" + mId + "Add mCurrentLimitOrder：  " + mCurOrderA.ToExcleString() + "CurAmount:" + mData.CurAmount);
+                if (mCurOrderA.Result == ExchangeAPIOrderResult.Canceled)
+                    mCurOrderA = null;
             }
             catch (Exception ex)
             {
@@ -421,10 +421,10 @@ namespace cuckoo_csharp.Strategy.Arbitrage
                 var orderResults = await mExchangeAAPI.PlaceOrdersAsync(requestA);
                 mCurOrderA = orderResults[0];
                 mOrderIds.Add(mCurOrderA.OrderId);
-                if (mCurOrderA.Result == ExchangeAPIOrderResult.Canceled)
-                    mCurOrderA = null;
                 Logger.Debug("mId:" + mId + "requestA：  " + requestA.ToString());
                 Logger.Debug("mId:" + mId + "Add mCurrentLimitOrder：  " + mCurOrderA.ToExcleString());
+                if (mCurOrderA.Result == ExchangeAPIOrderResult.Canceled)
+                    mCurOrderA = null;
             }
             catch (Exception ex)
             {
