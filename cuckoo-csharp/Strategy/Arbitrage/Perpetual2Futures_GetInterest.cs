a5bb1ce0a523bee6d080f1261cc25b4960d76d6d
liuqiba <874740127@qq.com>
2020-05-28 16:11:07 +0800

    修复 rest 提交订单还没返回 ,ws提前返回已经成交,这时候 误认为该订单不是自己提交的订单 问题

--- cuckoo-csharp/Strategy/Arbitrage/Perpetual2Futures_GetInterest.cs
+++ cuckoo-csharp/Strategy/Arbitrage/Perpetual2Futures_GetInterest.cs
@@ -46,6 +46,10 @@ namespace cuckoo_csharp.Strategy.Arbitrage
         /// </summary>
         private List<string> mOrderIds = new List<string>();
         /// <summary>
+        /// A交易所 历史成交订单详情
+        /// </summary>
+        private Dictionary<string, ExchangeOrderResult> mOrderResultsDic = new Dictionary<string, ExchangeOrderResult>();
+        /// <summary>
         /// A交易所止损止盈订单ID
         /// </summary>
         private List<string> mProfitOrderIds = new List<string>();
@@ -966,6 +970,9 @@ namespace cuckoo_csharp.Strategy.Arbitrage
                 var v = await mExchangeAAPI.PlaceOrderAsync(requestA);
                 mCurOrderA = v;
                 mOrderIds.Add(mCurOrderA.OrderId);
+                //检查如果已经成交的订单中有本订单号,那么启用成交逻辑
+                if (mOrderResultsDic.TryGetValue(mCurOrderA.OrderId,out ExchangeOrderResult filledOrder))
+                    OnOrderAHandler(filledOrder);
                 Logger.Debug(Utils.Str2Json("requestA", requestA.ToString()));
                 Logger.Debug(Utils.Str2Json("Add mCurrentLimitOrder", mCurOrderA.ToExcleString(), "CurAmount", mData.CurAAmount));
                 if (mCurOrderA.Result == ExchangeAPIOrderResult.Canceled)
@@ -1147,7 +1154,11 @@ namespace cuckoo_csharp.Strategy.Arbitrage
             if (order.MarketSymbol != mData.SymbolA)
                 return;
             if (!IsMyOrder(order.OrderId))
+            {
+                AddFilledOrderResult(order);
                 return;
+            }
+
             switch (order.Result)
             {
                 case ExchangeAPIOrderResult.Unknown:
@@ -1357,6 +1368,19 @@ namespace cuckoo_csharp.Strategy.Arbitrage
         {
             var s = 1 / mData.MinPriceUnit;
             return Math.Round(price * s) / s;
+        }
+        private void AddFilledOrderResult(ExchangeOrderResult orderRequest)
+        {
+            if (orderRequest.Result == ExchangeAPIOrderResult.Filled || orderRequest.Result == ExchangeAPIOrderResult.FilledPartially)
+            {
+                if (mOrderResultsDic.TryGetValue(orderRequest.OrderId, out ExchangeOrderResult lastResult))
+                {
+                    if (lastResult.Result != ExchangeAPIOrderResult.Filled)
+                    {
+                        mOrderResultsDic[orderRequest.OrderId] = orderRequest;
+                    }
+                }
+            }
         }
         /*
         /// <summary>
